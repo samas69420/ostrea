2be415d8fe43074d8c689ef80232e89369b52699
samas69420 <saveriothemonster@live.it>
2026-05-11 19:59:48 +0200

    frame stacking for racing env

--- src/envs/custom_car_racing.py
+++ src/envs/custom_car_racing.py
@@ -14,25 +14,34 @@ class CustomCarRacing(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"]}
 
 
-    def __init__(self,**kwargs):
-        super(CustomCarRacing).__init__()
+    def __init__(self, past_frames = 4, **kwargs):
+        super().__init__()
         self.env = gym.make("CarRacing-v3",**kwargs)
         self.action_space = self.env.action_space
-        self.observation_space = gym.spaces.Box(0,255,(3,96,96),np.uint8)
+        self.observation_space = gym.spaces.Box(0,1,(past_frames,96,96),np.float32)
+        self.obs = np.zeros((past_frames,96,96))
 
 
     def step(self,action):
-        observation, reward, terminated, truncated, info = self.env.step(action)
-        observation = observation.swapaxes(0,2)
-        observation = observation.swapaxes(1,2)
-        return (observation, reward, terminated, truncated, info)
+        frame, reward, terminated, truncated, info = self.env.step(action)
+        frame = frame.swapaxes(0,2)
+        frame = frame.swapaxes(1,2)
+        frame = frame.mean(axis=0, dtype=np.float32)
+        new_obs = np.zeros_like(self.obs, dtype=np.float32)
+        new_obs[1:] = self.obs[:-1]
+        new_obs[0] = frame
+        self.obs = new_obs
+        return (self.obs, reward, terminated, truncated, info)
 
 
     def reset(self,**kwargs):
-        observation, info = self.env.reset(**kwargs)
-        observation = observation.swapaxes(0,2)
-        observation = observation.swapaxes(1,2)
-        return (observation,info)
+        self.obs = np.zeros(self.observation_space.shape, dtype=np.float32)
+        frame, info = self.env.reset(**kwargs)
+        frame = frame.swapaxes(0,2)
+        frame = frame.swapaxes(1,2)
+        frame = frame.mean(axis=0, dtype=np.float32)
+        self.obs[0] = frame
+        return (self.obs,info)
 
 
     def render(self,**kwargs):
@@ -41,7 +50,8 @@ class CustomCarRacing(gym.Env):
 
 gym.register(
     id="CustomCarRacing",
-    entry_point="envs.custom_car_racing:CustomCarRacing")
+    entry_point="envs.custom_car_racing:CustomCarRacing",
+    max_episode_steps = 1000)
 
 
 if __name__ == "__main__":
